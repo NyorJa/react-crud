0d2cc63f88d46f0fb093bdfd090711b2824aae74
NyorJa <botsot.felix@gmail.com>
2024-02-25 17:59:03 +0800

    Remove extra config

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                 target: "http://localhost:9000",
-                 changeOrigin: true,
-                 secure: false,      
-                 ws: true,
-                 rewrite: (path) => path.replace(/^\/api/, "/api"),
+                 target: "http://localhost:9000"
              }
         }
     }
